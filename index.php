7057459718e8b3b50dacff713431b139ccff5041
crlstve <ecarles10@gmail.com>
2024-10-15 22:01:56 +0200

    init

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * WP Play Profile
+ *
+ * @package       wp-play-profile
+ * @author        c.esteve
+ *
+ * @wordpress-plugin
+ * Plugin Name:   WP Play Profile
+ * Plugin URI:   https://carlesteve.dev
+ * Description:  Get data from psn network profile.
+ * Version:      0.0.1
+ * Author:        c.esteve
+ * Author URI:   https://carlesteve.dev
+ * Update URI:   https://carlesteve.dev
+ * Text Domain:   wp-play-profile
+ * Domain Path:   /lang
+ */
+
+    if ( ! defined( 'ABSPATH' ) ) {
+        exit; // Exit if accessed directly.
+    }
+?>
